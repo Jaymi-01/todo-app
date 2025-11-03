653685ab37e00bd4c9306bb76fcfbac316e2ab5e
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-11-04 00:21:38 +0100

    fixed the index showing qat the top of the screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,20 @@
-import { ConvexProvider, ConvexReactClient } from 'convex/react';
-import { Stack } from 'expo-router';
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { Stack } from "expo-router";
 
-const convexUrl = process.env.EXPO_PUBLIC_CONVEX_URL || 'https://cool-meadowlark-411.convex.cloud';
+const convexUrl =
+  process.env.EXPO_PUBLIC_CONVEX_URL ||
+  "https://cool-meadowlark-411.convex.cloud";
 
 const convex = new ConvexReactClient(convexUrl);
 
 export default function RootLayout() {
   return (
     <ConvexProvider client={convex}>
-      <Stack />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
     </ConvexProvider>
   );
 }
